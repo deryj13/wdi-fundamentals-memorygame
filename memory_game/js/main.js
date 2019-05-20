d31176cce20d8de85d7f8ff6893f69ab855a8338
Dery J <dery.j13@gmail.com>
2019-05-20 19:43:07 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,24 @@
 const cards = ["Queen", "Queen", "King", "King"];
 
-cardsInPlay = [];
-const cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-const cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+const cardsInPlay = [];
+function checkForMatch() {
+		if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+			console.log("You found a match!");
+		} 
+		else {
+			alert("Sorry, try again!");
+			console.log("Sorry, try again!");
+		}
+	}
+}
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
 
-if (cardsInPlay.length === 2) {
-     if (cardsInPlay[0] === cardsInPlay[1]) {
-     	alert("You found a match!");
-     } else {
-     	alert("Sorry, try again!");
-     }
- }
-
-
-
-
-
+flipCard(0);
+flipCard(1);
 
